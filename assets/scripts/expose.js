801a321052a3db700b61f3ca06bc7876e1bba7a9
Qiyun506 <83425380+Qiyun506@users.noreply.github.com>
2022-10-30 22:08:07 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,33 +3,31 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  const sound = document.querySelector('audio');
-  const but = document.querySelector('button');
+  const aud = document.querySelector('audio');
   const horn = document.getElementById('horn-select');
-  const vol = document.getElementById('volume-controls');
-  const pic = document.querySelector('img[alt="No image selected"]');
-  const VC = document.getElementById('volume-controls');
-//-------------------------------declare----------------------------------------
-  const JC = new JSConfetti();
+  const img = document.querySelector('img[alt="No image selected"]');
+  const jsConfetti = new JSConfetti()
   horn.addEventListener('change', (event) => {
-    pic.src = `assets/images/${event.target.value}.svg`;
-    sound.src = `assets/audio/${event.target.value}.mp3`;
+    img.src = `assets/images/${event.target.value}.svg`;
+    aud.src = `assets/audio/${event.target.value}.mp3`;
   });
-  but.addEventListener('click', (event) => {
+  const vol_control = document.getElementById('volume-controls');
+  vol_control.addEventListener('change', (event) => {
+    let curr_vol = event.target.value;
+    let vol_lv = 0;
+    if (curr_vol == 0) vol_lv = 0
+    else if (curr_vol < 33) vol_lv = 1
+    else if (curr_vol < 67) vol_lv = 2
+    else vol_lv = 3;
+    const vol_icon = vol_control.querySelector('img');
+    vol_icon.src = `assets/icons/volume-level-${vol_lv}.svg`
+    aud.volume = curr_vol / 100.0;
+  })
+  const button = document.querySelector('button');
+  button.addEventListener('click', (event) => {
     if (horn.value == 'party-horn'){
-      JC.addConfetti();
+      jsConfetti.addConfetti();
     }
     aud.play();
   });
-  VC.addEventListener('change', (event) => {
-    let curr = event.target.value;
-    let vol_lv = 0;
-    if (curr  == 0) vol_lv = 0
-    else if (curr  < 34) vol_lv = 1
-    else if (curr  < 66) vol_lv = 2
-    else vol_lv = 3;
-    const icon = vol_control.querySelector('img');
-    icon.src = `assets/icons/volume-level-${vol_lv}.svg`
-    aud.volume = curr  / 100.0;
-  })
 }
